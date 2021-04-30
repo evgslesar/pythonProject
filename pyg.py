4a7ebf8361cb0a819c46828a08cd39cf6a2e0d6f
evg <evgsles@yandex.ru>
2021-04-30 19:43:33 +0800

    bivis

--- pyg.py
+++ pyg.py
@@ -1,19 +1,88 @@
-import pygame
+# -*- coding: utf-8 -*-
 
-pygame.init()
-screen = pygame.display.set_mode([800, 600])
+from random import randint
+from termcolor import cprint
 
-keep_going = True
-GREEN = (0, 255, 0)
+class Man:
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
 
-radius = 50
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(self.name, self.fullness)
 
-while keep_going:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            keep_going= False
-    pygame.draw.circle(screen, GREEN, (100, 100), radius)
-    pygame.display.update()
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
 
-pygame.quit()
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 50
+        self.fullness -= 10
 
+    def watch_MTV(self):
+        cprint('{} смотрел MTV весь день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} вьехал в дом'.format(self.name), color='cyan')
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watch_MTV()
+
+class House:
+    def __init__(self):
+        self.food = 50
+        self.money = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}'.format(self.food, self.money)
+
+citizens = [
+    Man(name='Бивис'),
+    Man(name='Батхед'),
+    Man(name='Кенни'),
+]
+
+my_sweet_home = House()
+for citizen in citizens:
+    citizen.go_to_the_house(house=my_sweet_home)
+
+for day in range(1, 366):
+    print('=================== день {} ====================='.format(day))
+    for citizen in citizens:
+        citizen.act()
+    print('---------- в конце дня -----------')
+    for citizen in citizens:
+        print(citizen)
+    print(my_sweet_home)

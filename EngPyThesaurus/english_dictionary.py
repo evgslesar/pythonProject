a6e3d69deda9f48478f2efd90f7feae90ac9e0c8
evg <evgsles@yandex.ru>
2021-07-01 23:38:51 +0800

    Rename to eng. dict

---
+++ EngPyThesaurus/english_dictionary.py
@@ -0,0 +1,16 @@
+import json
+
+data = json.load(open('data.json'))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return 'The word does not exists. Please double check it.'
+
+word = input("Enter word: ")
+
+print(translate(word))
+
+

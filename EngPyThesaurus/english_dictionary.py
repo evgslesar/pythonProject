f14ce80ef44e8ccfe3246d0925f32c4ca82ca457
evg <evgsles@yandex.ru>
2021-07-02 00:08:11 +0800

    eng. dict final

--- EngPyThesaurus/english_dictionary.py
+++ EngPyThesaurus/english_dictionary.py
@@ -20,6 +20,12 @@ def translate(w):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
 
 

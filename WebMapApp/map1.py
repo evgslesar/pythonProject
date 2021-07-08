feba1c701bfd42e0a307d7e47a51dfb492b1a3d3
evg <evgsles@yandex.ru>
2021-07-09 01:02:33 +0800

    Volcanoes map with colored markers

--- WebMapApp/map1.py
+++ WebMapApp/map1.py
@@ -4,13 +4,23 @@ data = pandas.read_csv("Volcanoes.txt")
 
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles='Stamen Terrain')
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup='Some Marker', icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=str(el)+" m",
+                            fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("Map1.html")

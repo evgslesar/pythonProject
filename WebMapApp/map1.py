366094d9fc6ea2641d96ab83c7f84824886b29bd
evg <evgsles@yandex.ru>
2021-07-09 14:48:00 +0800

    Volcanoes map with population and control pan

--- WebMapApp/map1.py
+++ WebMapApp/map1.py
@@ -1,7 +1,7 @@
 import folium
 import pandas
-data = pandas.read_csv("Volcanoes.txt")
 
+data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
@@ -14,13 +14,23 @@ def color_producer(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles='Stamen Terrain')
+map = folium.Map(location=[38.58, -99.09], zoom_start=3, tiles='Stamen Terrain')
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
+fgp = folium.FeatureGroup(name="Population")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=str(el)+" m",
-                            fill_color=color_producer(el), color='grey', fill_opacity=0.7))
-map.add_child(fg)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=str(el)+" m",
+                    fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+    style_function=lambda x: {'fillColor': 'green'
+    if x['properties']['POP2005'] < 10000000 else 'orange'
+    if 10000000 <= x['properties']['POP2005'] < 30000000 else 'blue'
+    if 30000000 <= x['properties']['POP2005'] < 100000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")

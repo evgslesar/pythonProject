eac7c9693762349f64f54e476bd84a69a9e65152
evg <evgsles@yandex.ru>
2021-07-17 01:24:20 +0800

    Parser hh unfinished

---
+++ pars/headhunter.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+ITEMS = 100
+URL = f'https://hh.ru/search/vacancy?st=searchVacancy&text=python&items_on_page={ITEMS}'
+
+headers = {
+  'Host': 'hh.ru',
+  'User-Agent': 'Safari',
+  'Accept': '*/*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive'
+}
+
+def extract_max_page():
+
+  hh_requests = requests.get(URL, headers=headers)
+
+  hh_soup = BeautifulSoup(hh_requests.text, 'html.parser')
+
+  pages = []
+  paginator = hh_soup.find_all("span", {'class': 'pager-item-not-in-short-range'})
+
+  for page in paginator:
+    pages.append(int(page.find('a').text))
+
+  return pages[-1]
+
+def extract_hh_jobs(last_page):
+  jobs = []
+  # for page in range(last_page):
+  result = requests.get(f'{URL}&page=0', headers=headers)
+  print(result.status_code)
+  soup = BeautifulSoup(result.text, 'html.parser')
+  results = soup.find_all('div', {'class': 'vacancy-serp-item'})
+  for result in results:
+    title = result.find('a').text
+    company = result.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
+    print(company)
+
+  return jobs
---
+++ pars/hh_parsvac.py
@@ -0,0 +1,6 @@
+from headhunter import extract_max_page, extract_hh_jobs
+'''
+Works in repl.it? doesn't work on local PC
+'''
+max_page = extract_max_page()
+extract_hh_jobs(max_page)

a38258763d33c2d07daeb473ce3023efced32b1e
evg <evgsles@yandex.ru>
2021-07-17 23:47:40 +0800

    ready pars module for hh

--- pars/headhunter.py
+++ pars/headhunter.py
@@ -13,29 +13,34 @@ headers = {
 }
 
 def extract_max_page():
-
   hh_requests = requests.get(URL, headers=headers)
-
   hh_soup = BeautifulSoup(hh_requests.text, 'html.parser')
-
   pages = []
   paginator = hh_soup.find_all("span", {'class': 'pager-item-not-in-short-range'})
-
   for page in paginator:
     pages.append(int(page.find('a').text))
-
   return pages[-1]
+
+def extract_job(html):
+  title = html.find('a').text
+  link = html.find('a')['href']
+  company = html.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).text
+  company = company.strip()
+  location = html.find('span', {'data-qa': 'vacancy-serp__vacancy-address'}).text
+  location = location.partition(',')[0]
+  return {'title': title, 'company': company, 'location': location, 'link': link}
+
 
 def extract_hh_jobs(last_page):
   jobs = []
-  # for page in range(last_page):
-  result = requests.get(f'{URL}&page=0', headers=headers)
-  print(result.status_code)
-  soup = BeautifulSoup(result.text, 'html.parser')
-  results = soup.find_all('div', {'class': 'vacancy-serp-item'})
-  for result in results:
-    title = result.find('a').text
-    company = result.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
-    print(company)
+  for page in range(last_page):
+    print(f'Парсинг страницы {page}')
+    result = requests.get(f'{URL}&page={page}', headers=headers)
+    print(result.status_code)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    results = soup.find_all('div', {'class': 'vacancy-serp-item'})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
 
   return jobs
--- pars/hh_parsvac.py
+++ pars/hh_parsvac.py
@@ -1,6 +1,8 @@
 from headhunter import extract_max_page, extract_hh_jobs
 '''
-Works in repl.it? doesn't work on local PC
+Works in repl.it doesn't work on local PC
 '''
-max_page = extract_max_page()
-extract_hh_jobs(max_page)
+hh_max_page = extract_max_page()
+hh_jobs = extract_hh_jobs(hh_max_page)
+
+print(hh_jobs)

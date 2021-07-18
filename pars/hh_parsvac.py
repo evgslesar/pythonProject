4ff690637d8165329d39a18b6c7640c81df4e764
evg <evgsles@yandex.ru>
2021-07-18 13:28:41 +0800

    Saving to csv

--- pars/hh_parsvac.py
+++ pars/hh_parsvac.py
@@ -1,13 +1,23 @@
 from headhunter import get_jobs as hh_get_jobs
 from so import get_jobs as so_get_jobs
+import csv
 
 '''
-With hh.ru works in repl.it doesn't work on local PC
+With hh.ru it works in repl.it doesn't work on local PC
 '''
+
+def save_to_csv(jobs):
+  file = open('test.csv', mode='w')
+  writer = csv.writer(file)
+  writer.writerow(['title', 'company', 'location', 'link'])
+  for job in jobs:
+    writer.writerow(list(job.values()))
+  return
+
 
 hh_jobs = hh_get_jobs()
 so_jobs = so_get_jobs()
 
 jobs = hh_jobs + so_jobs
 
-print(jobs)
+save_to_csv(jobs)

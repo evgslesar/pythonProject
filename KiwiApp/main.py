0ccd7289914f6baccbe0845058f2cf1278558feb
evg <evgsles@yandex.ru>
2021-08-16 01:06:19 +0800

    kivy app continue

--- KiwiApp/main.py
+++ KiwiApp/main.py
@@ -1,5 +1,8 @@
+import glob
 import json
 from datetime import datetime
+from pathlib import Path
+import random
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -46,6 +49,18 @@ class LoginSuccessScreen(Screen):
         self.manager.transition.direction = 'right'
         self.manager.current = "login_screen"
 
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+        available_feelings = [Path(filename).stem for filename in
+        available_feelings]
+
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt", encoding='utf-8') as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+             self.ids.quote.text = "Try another feeling"
 
 class MainApp(App):
     def build(self):

fae12a1a797f9a0ec116e51d47531f65f6647678
evg <evgsles@yandex.ru>
2021-08-24 01:25:29 +0800

    motion detect

--- ParsOlMol/main.py
+++ ParsOlMol/main.py
@@ -1,51 +1,96 @@
+import re
 import csv
+
 import requests
-from multiprocessing import Pool
-from time import sleep
-
-
-def get_html(url):
-    # sleep(1)
-    r = requests.get(url)
-    return r.text
+from bs4 import BeautifulSoup
 
 
 def write_csv(data):
-    with open('websites.csv', 'a', newline='', encoding='utf-8') as f:
-        order = ['name', 'url', 'description', 'traffic', 'percent']
-        writer = csv.DictWriter(f, fieldnames=order)
+    with open('result.csv', 'a', encoding='utf-8') as f:
+        fields = ['title', 'reviews', 'released', 'tags']
+        writer = csv.DictWriter(f, fieldnames=fields)
         writer.writerow(data)
 
 
-def get_page_data(text):
-    data = text.strip().split('\n')[1:]
+def get_html(url):
+    response = requests.get(url)
+    if not response.ok:
+        print(f'Code: {response.status_code}, url: {url}')
+    return response.text
 
-    for row in data:
-        columns = row.strip().split('\t')
-        name = columns[0]
-        url = columns[1]
-        description = columns[2]
-        traffic = columns[3]
-        percent = columns[4]
 
-        data = {'name': name,
-                'url': url,
-                'description': description,
-                'traffic': traffic,
-                'percent': percent}
-        write_csv(data)
+def get_games(html):
+    soup = BeautifulSoup(html, 'lxml')
+    pattern = r'^https://store.steampowered.com/app/'
+    games = soup.find_all('a', href=re.compile(pattern))
+    return games
 
-def make_all(url):
-    text = get_html(url)
-    get_page_data(text)
-    
+
+def get_hover_data(id):
+    url = f'https://store.steampowered.com/apphoverpublic/{id}'
+    html = get_html(url)
+    soup = BeautifulSoup(html, 'lxml')
+
+    try:
+        title = soup.find('h4', class_='hover_title').text.strip()
+    except:
+        title = ''
+        print(url)
+
+    try:
+        released = soup.find('div', class_='hover_release').span.text.split(':')[-1].strip()
+    except:
+        released = ''
+        print(url)
+
+    try:
+        reviews_raw = soup.find('div', class_='hover_review_summary').text
+    except:
+        reviews = ''
+        print(url)
+    else:
+        pattern = r'\d+'
+        reviews = int(''.join(re.findall(pattern, reviews_raw)))
+
+    try:
+        tags_raw = soup.find_all('div', class_='app_tag')
+    except:
+        tags = ''
+        print(url)
+    else:
+        tags_text = [tag.text for tag in tags_raw]
+        tags = ', '.join(tags_text)
+
+    data = {
+        'title': title,
+        'released': released,
+        'reviews': reviews,
+        'tags': tags
+    }
+
+    write_csv(data)
+
 
 def main():
-    url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page={}'
-    urls = [url.format(str(i)) for i in range(1, 4600)]
+    all_games = []
+    start = 0
+    url = f'https://store.steampowered.com/search/results/?query&start={start}&count=100&tags=1702'
 
-    with Pool(20) as p:
-        p.map(make_all, urls)
+    while True:
+
+        games = get_games(get_html(url))
+
+        if games:
+            all_games.extend(games)
+            start += 100
+            url = f'https://store.steampowered.com/search/results/?query&start={start}&count=100&tags=1702'
+        else:
+            break
+
+    for game in all_games:
+        id = game.get('data-ds-appid')
+        get_hover_data(id)
+
 
 if __name__ == '__main__':
     main()
